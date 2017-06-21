d851c886702a0a8a3ee175bffa6213118a50bc29
Barb King <barbgross@aol.com>
2017-06-20 23:39:51 -0500

    prime number challenge

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -14,3 +14,14 @@ function findSmallestNum(arr) {
 }
 var array = [34,-345,-1,100,100];
 findSmallestNum(array);
+
+//Create a function that returns true if a number is a prime, and false if it is not.
+//A prime is a natural number that is only divisible by 1 and itself.
+function isPrime(num) {
+    for(var i = 2; i < num; i++) {
+        if(num % i === 0) {
+            return false;
+        }
+    }
+    return num > 1;
+}
